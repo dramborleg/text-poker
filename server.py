2bea39f58a1a7028b26f4f5114c22ce10e5d4ee7
Colin Lord <cdlord2@illinois.edu>
2016-07-17 17:58:24 -0400

    Add simple cherrypy server to interface via sms
    Adds a simple cherrypy server setup that will allow the game to be played
    via texting. As of now the server only receives a message and responds with
    a constant reply, but once more framework is set up it will be easy to get
    the replies from the game engine itself.

---
+++ server.py
@@ -0,0 +1,18 @@
+import cherrypy
+import twilio.twiml
+import configparser
+
+class Processor(object):
+    @cherrypy.expose
+    def index(self, **kwargs):
+        resp = twilio.twiml.Response()
+        resp.message('HelloWorld')
+        return str(resp)
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('server.conf')
+    settings = config['Server']
+    cherrypy.config.update({'server.socket_host': settings['IP_ADDRESS'],
+                            'server.socket_port': int(settings['PORT'])})
+    cherrypy.quickstart(Processor())
