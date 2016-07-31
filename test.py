73e4cb740d23e706676844f51051e8c6b38b19ca
Colin Lord <cdlord2@illinois.edu>
2016-07-31 16:50:45 -0400

    Add tests for joining games and setting tags
    Adds tests for the --tag and --join options, as well as modify the
    testing framework to be just a little bit more flexible to work with.

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import parser
 
-p = parser.Parser()
-input = ['-c mango', '--create mango', 'c mango', '-c Uguisu',
-         '--create kiwi c guava lemon', '--create']
+game_creation = [('-c mango', 0), ('--create mango', 0), ('c mango', 0),
+                 ('-c Uguisu', 0), ('--create kiwi c guava lemon', 0),
+                 ('--create GAME_4', 0), ('-c', 0)]
+add_players = [('-c mango', 0), ('-j', 0), ('--join mango', 0), ('j mango', 0),
+               ('-j mango', 'dramborleg'), ('-j mang0', 1), ('-j mango', 0),
+               ('-t ringil', 'dramborleg'), ('-t kiwi', 1), ('-j mango', 1)]
 
-for i in input:
-    ret = p.parse(i, 0)
-    print('--------')
-    print(i)
-    print(ret)
-    print('--------')
+tests = [('GAME CREATION', game_creation), ('ADDING PLAYERS', add_players)]
+
+for test in tests:
+    p = parser.Parser()
+    print('TESTING TEST: %s' % test[0])
+    for i in test[1]:
+        ret = p.parse(i[0], i[1])
+        print('--------')
+        print(i)
+        print(ret)
+        print('--------')
